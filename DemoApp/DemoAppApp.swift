329399829109c9e2f00a2533f15918ec5f51ac0a
Nagendra Patel <nagendra.k.patel@gsk.com>
2023-08-31 10:06:17 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Nagendra Patel  on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
